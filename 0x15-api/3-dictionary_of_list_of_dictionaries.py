97db02533cddba1fd612beeb596a8378b460f7b8
jorgechauxjr <chauxjorge@hotmail.com>
2020-05-05 04:08:11 +0000

    Fix 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,38 +3,31 @@
 
 import json
 import requests
-from sys import argv
-
-url = 'https://jsonplaceholder.typicode.com/'
-
-
-def get_username(user_id):
-    """Return the username"""
-
-    user_request = "{}users/{}".format(url, user_id)
-    employee_dict = requests.get(user_request).json()
-    employee_username = employee_dict.get("username")
-    return employee_username
 
 
 def search_and_export_json():
     """Export data in the JSON format"""
 
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    users_dict = requests.get("{}users".format(url)).json()
     tasks_dict = requests.get("{}todos".format(url)).json()
 
     my_dict = {}
-    my_list = []
+    usernames_dict = {}
+
+    for info in users_dict:
+        user_id = info.get('id')
+        my_dict[user_id] = []
+        usernames_dict[user_id] = info.get('username')
 
     for info in tasks_dict:
         tasks_all_emp = {}
+        user_id = info.get('userId')
         tasks_all_emp["task"] = info.get('title')
         tasks_all_emp["completed"] = info.get('completed')
-        id = info.get('userId')
-        username = get_username(id)
-        tasks_all_emp["username"] = username
-        my_list.append(tasks_all_emp)
-
-    my_dict["1"] = my_list
+        tasks_all_emp["username"] = usernames_dict.get(user_id)
+        my_dict.get(user_id).append(tasks_all_emp)
 
     file_name = "todo_all_employees.json"
     with open(file_name, mode='w') as json_file:
