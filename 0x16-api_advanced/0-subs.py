06758f91dbfa219d48e108602454604c37196f81
jorgechauxjr <chauxjorge@hotmail.com>
2020-05-07 20:58:50 +0000

    Add 0-sub

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A file to make a query to an endpoint
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """A function that queries the Reddit API and returns the number
+    of subscribers"""
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    headers = {"User-Agent": "Python3"}
+    response = requests.request("GET", url, headers=headers).json()
+    try:
+        subscribers = response['data']['subscribers']
+        return subscribers
+    except Exception:
+        return 0
