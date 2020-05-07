fa6952d0631d5c10100618a8047bbc3268d5f56a
jorgechauxjr <chauxjorge@hotmail.com>
2020-05-07 21:30:52 +0000

    Add 1-top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A file to make a query to an endpoint
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts"""
+    url = "https://api.reddit.com/r/{}/top/.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "Python3"}
+    response = requests.request("GET", url, headers=headers).json()
+    try:
+        for i in range(10):
+            print(response['data']['children'][i]['data']['title'])
+    except Exception:
+        return 0
