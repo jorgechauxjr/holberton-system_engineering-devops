92d01e50ec437e844bca7d8fe1c51880e3c15429
jorgechauxjr <chauxjorge@hotmail.com>
2020-05-28 19:59:40 +0000

    Fix 0the sky

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
-# modify file
-# execute sed command in shell. Independet of the path
+# Modify file
 
-exec {'Modify open files':
-     command  => 'sed -i "s/-n 15/-n 2500/g" /etc/default/nginx',
-     provider => 'shell'
+exec { 'Modify file':
+    command  => 'sed -i "s/-n 15/ -n 30000/g" /etc/default/nginx',
+    provider => 'shell'
 }
-exec {'restart nginx':
-     command  => 'service nginx restart',
-     provider => 'shell'
+
+exec { 'Restart nginx':
+    command  => 'service nginx restart',
+    provider => 'shell'
 }
